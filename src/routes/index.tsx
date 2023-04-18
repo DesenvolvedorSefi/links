b41a4c6e8b1223d06609f7bb3dcde107ed0fcb10
Anderson Sousa <and3rsonsousa@outlook.com>
2023-04-18 18:45:43 -0300

    start

---
+++ src/routes/index.tsx
@@ -0,0 +1,61 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+
+export default component$(() => {
+	return (
+		<div class="max-w-md flex justify-between flex-col gap-8 p-8">
+			<div class="mb-4">
+				<img src="/logo.png" class="mx-auto h-24" alt="" />
+			</div>
+			<div class="links">
+				<a
+					href="https://contadeluz.seficredito.com.br"
+					class="link link-primary"
+				>
+					Empréstimo na Conta de Energia
+				</a>
+
+				<a href="https://fgts.seficredito.com.br" class="link">
+					<span class="sm:block hidden">
+						Antecipe até 10 parcelas do seu FGTS
+					</span>
+					<span class="sm:hidden">Antecipe seu FGTS</span>
+				</a>
+
+				<a href="https://wa.me/5588993432811" class="link">
+					Negativado? Aqui tem crédito
+				</a>
+				<a href="https://seficredito.com.br" class="link">
+					Nosso site
+				</a>
+				<a href="https://wa.me/5588993432811" class="link">
+					Fale conosco
+				</a>
+			</div>
+			{/* <div class="grid grid-cols-3">
+				<div>
+
+				</div>
+				<div></div>
+				<div></div>
+			</div> */}
+			<div class="font-black text-center">
+				SEFI{" "}
+				<span class="font-medium text-sm">
+					{" "}
+					É fácil. É rápido. É SEFI.
+				</span>
+			</div>
+		</div>
+	);
+});
+
+export const head: DocumentHead = {
+	title: "SEFI - É fácil. É rápido. É SEFI.",
+	meta: [
+		{
+			name: "description",
+			content: "A maneira mais fácil de fazer o seu empréstimo é aqui.",
+		},
+	],
+};
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+	theme: {
+		extend: {
+			colors: {
+				brand: "#007DF9",
+				accent: "#E6D519",
+				support: "#083691",
+			},
+		},
+	},
+	plugins: [],
+};
