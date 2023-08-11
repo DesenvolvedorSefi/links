fe68a2fe10bf4000b31c8966081ebc9e365a8e0d
DesenvolvedorSefi <118212737+DesenvolvedorSefi@users.noreply.github.com>
2023-08-11 10:14:19 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -22,13 +22,13 @@ export default component$(() => {
 					<span class="sm:hidden">Antecipe seu FGTS</span>
 				</a>
 
-				<a href="https://wa.me/5588981468496" class="link">
+				<a href="https://wa.me/5588993432811" class="link">
 					Negativado? Aqui tem crédito
 				</a>
 				<a href="https://seficredito.com.br" class="link">
 					Nosso site
 				</a>
-				<a href="https://wa.me/5588981468496" class="link">
+				<a href="https://wa.me/5588993432811" class="link">
 					Fale conosco
 				</a>
 			</div>
